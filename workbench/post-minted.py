333995c5f4f38c16ebf502f6c4c62a1cdda217b6
LAURENS Jérôme <jerome.laurens@u-bourgogne.fr>
2022-01-21 19:51:02 +0100

    workbench

---
+++ workbench/post-minted.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import fileinput
+import sys
+
+
+if len(sys.argv) < 4:
+  raise ValueError(f'Need three args at least, given {len(sys.argv)}')
+
+fin = sys.argv[1]
+wrapper = sys.argv[2]
+print(f'File: {fin}')
+with fileinput.input(fin, inplace=True) as file:
+  for cmd in sys.argv[3:]:
+    pattern_find = fR'{{\PYGZbs{{}}{cmd}}}'
+    pattern_sub = fR'{{\{wrapper}{{\PYGZbs{{}}{cmd}}}}}'
+    for line in file:
+      print(line.replace(pattern_find, pattern_sub), end='')
